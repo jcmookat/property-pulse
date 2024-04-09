5f379e8f7957737aa07d747472c018ebdc486b9a
Jose Cardenas <jscardenas717@rocketmail.com>
2024-04-09 22:26:15 +0900

    Sign Out and Protect Routes with middleware

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -195,7 +195,10 @@ const Navbar = () => {
 										className='block px-4 py-2 text-sm text-gray-700'
 										role='menuitem'
 										tabIndex='-1'
-										id='user-menu-item-0'>
+										id='user-menu-item-0'
+										onClick={() => {
+											setIsProfileMenuOpen(false);
+										}}>
 										Your Profile
 									</Link>
 									<Link
@@ -203,10 +206,17 @@ const Navbar = () => {
 										className='block px-4 py-2 text-sm text-gray-700'
 										role='menuitem'
 										tabIndex='-1'
-										id='user-menu-item-2'>
+										id='user-menu-item-2'
+										onClick={() => {
+											setIsProfileMenuOpen(false);
+										}}>
 										Saved Properties
 									</Link>
 									<button
+										onClick={() => {
+											setIsProfileMenuOpen(false);
+											signOut();
+										}}
 										className='block px-4 py-2 text-sm text-gray-700'
 										role='menuitem'
 										tabIndex='-1'
