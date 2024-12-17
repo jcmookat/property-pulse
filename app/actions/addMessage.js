7c1ed5aac69f2972cafe9b47709266097494024a
Jose Cardenas <jscardenas717@rocketmail.com>
2024-12-17 22:21:10 +0900

    Submit Message & useFormState Hook

--- app/actions/addMessage.js
+++ app/actions/addMessage.js
@@ -3,7 +3,8 @@ import connectDB from '@/config/database';
 import Message from '@/models/Message';
 import { getSessionUser } from '@/utils/getSessionUser';
 
-async function addMessage(formData) {
+async function addMessage(previousState, formData) {
+	// formData should be second argument
 	await connectDB();
 
 	const sessionUser = await getSessionUser();
@@ -32,7 +33,7 @@ async function addMessage(formData) {
 
 	await newMessage.save();
 
-	return { submitted: true };
+	return { submitted: true, successMessage: 'Message sent successfully' };
 }
 
 export default addMessage;
