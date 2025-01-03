9e1630b990fc048cafd2ef4f9ac99f62425677b6
Jose Cardenas <jscardenas717@rocketmail.com>
2025-01-03 11:00:31 +0900

    Photo lightbox

--- components/PropertyImages.jsx
+++ components/PropertyImages.jsx
@@ -1,44 +1,68 @@
+'use client';
+import { Gallery, Item } from 'react-photoswipe-gallery';
 import Image from 'next/image';
 
 const PropertyImages = ({ images }) => {
 	return (
-		<section className='bg-blue-50 p-4'>
-			<div className='container mx-auto'>
-				{images.length === 1 ? (
-					<div className='col-span-1'>
-						<Image
-							src={images[0]}
-							alt=''
-							className='object-cover h-[400px] w-full mx-auto rounded-xl'
-							width={0}
-							height={0}
-							sizes='100vw'
-							priority={true}
-						/>
-					</div>
-				) : (
-					<div className='grid grid-cols-2 gap-4'>
-						{images.map((image, i) => (
-							<div
-								key={i}
-								className={`
+		<Gallery>
+			<section className='bg-blue-50 p-4'>
+				<div className='container mx-auto'>
+					{images.length === 1 ? (
+						<div className='col-span-1'>
+							<Item
+								original={images[0]}
+								thumbnail={images[0]}
+								width='1000'
+								height='600'>
+								{({ ref, open }) => (
+									<Image
+										src={images[0]}
+										alt=''
+										ref={ref}
+										onClick={open}
+										className='object-cover h-[400px] w-full mx-auto rounded-xl cursor-pointer'
+										width={0}
+										height={0}
+										sizes='100vw'
+										priority={true}
+									/>
+								)}
+							</Item>
+						</div>
+					) : (
+						<div className='grid grid-cols-2 gap-4'>
+							{images.map((image, i) => (
+								<div
+									key={i}
+									className={`
                 ${images.length === 3 && i === 2 ? 'col-span-2' : 'col-span-1'}
               `}>
-								<Image
-									src={image}
-									alt=''
-									className='object-cover h-[400px] w-full rounded-xl'
-									width={0}
-									height={0}
-									sizes='100vw'
-									priority={true}
-								/>
-							</div>
-						))}
-					</div>
-				)}
-			</div>
-		</section>
+									<Item
+										original={image}
+										thumbnail={image}
+										width='1000'
+										height='600'>
+										{({ ref, open }) => (
+											<Image
+												src={image}
+												alt=''
+												ref={ref}
+												onClick={open}
+												className='object-cover h-[400px] w-full rounded-xl cursor-pointer'
+												width={0}
+												height={0}
+												sizes='100vw'
+												priority={true}
+											/>
+										)}
+									</Item>
+								</div>
+							))}
+						</div>
+					)}
+				</div>
+			</section>
+		</Gallery>
 	);
 };
 export default PropertyImages;
